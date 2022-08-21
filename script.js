d663126c725b08d2337c80a8c6b6418e869390a4
safouanaou <aouezgharsafouan@gmail.com>
2022-08-21 14:02:45 +0100

    changed the if statement that creates a dynamic grid size into a more consize method by implementing js varibale values inside css style using backticks method

--- script.js
+++ script.js
@@ -2,44 +2,18 @@ let body = document.getElementsByTagName("body")[0];
 
 let container = document.createElement("div");
     container.className = 'container';
-   body.appendChild(container);
+    body.appendChild(container);
 
+let buttons = document.querySelectorAll('button');
 
-
-
-    let buttons = document.querySelectorAll('button');
-
+// dynamic grid size
     buttons.forEach((button)=>{
         button.addEventListener('click', ()=>{
             let size = Number(button.className);
+            let squareNum = Math.sqrt(size);
 
-            if(size == 64){
-                container.style.gridTemplateColumns = 'repeat(8, 1fr)';
-                container.style.gridTemplateRows = 'repeat(8, 1fr)';
-            }else if(size == 256){
-                container.style.gridTemplateColumns = 'repeat(16, 1fr)';
-                container.style.gridTemplateRows = 'repeat(16, 1fr)';
-            }else if(size == 576){
-                container.style.gridTemplateColumns = 'repeat(24, 1fr)';
-                container.style.gridTemplateRows = 'repeat(24, 1fr)';
-            }else if(size == 1024){
-                container.style.gridTemplateColumns = 'repeat(32, 1fr)';
-                container.style.gridTemplateRows = 'repeat(32, 1fr)';
-            }else if(size == 1600){
-                container.style.gridTemplateColumns = 'repeat(40, 1fr)';
-                container.style.gridTemplateRows = 'repeat(40, 1fr)';
-            }else if(size == 2304){
-                container.style.gridTemplateColumns = 'repeat(48, 1fr)';
-                container.style.gridTemplateRows = 'repeat(48, 1fr)';
-            }else if(size == 3136){
-                container.style.gridTemplateColumns = 'repeat(56, 1fr)';
-                container.style.gridTemplateRows = 'repeat(56, 1fr)';
-            }else if(size == 4096){
-                container.style.gridTemplateColumns = 'repeat(64, 1fr)';
-                container.style.gridTemplateRows = 'repeat(64, 1fr)';
-            }
-
-
+            container.style.gridTemplateColumns = `repeat(${squareNum}, 1fr)`;
+            container.style.gridTemplateRows = `repeat(${squareNum}, 1fr)`;
 
             for (let i = 0; i < size; i++) {
                 let square = document.createElement('div');
